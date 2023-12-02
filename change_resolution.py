64ffddcea9632c85a5d82684440acbe000c84f9f
Martin Schulze <mail.martin.schulze@gmx.net>
2023-12-02 01:33:22 +0100

    Rewrite with Python3

--- change_resolution.py
+++ change_resolution.py
@@ -1,218 +1,229 @@
 # coding: latin-1
-import Tkinter as Tk
-import mmap
-import os
+
+import sys
+from pathlib import Path
+import logging
+import configparser
+
 import shutil
-import re
-import Image
-from tkFileDialog   import askopenfilename      
-class App:
-	def callbackfile(self):
-		fn=askopenfilename() 		
-		self.logit("check file: "+fn+"\n")
-		if not os.path.isfile(fn):
-			self.logit("no file"+fn+"\n")
-		else:
-			f=open(fn, "r+b")
-			map = mmap.mmap(f.fileno(), 0)
-			hexstr=" "+" ".join(["%02X"%ord(x) for x in map])
-			check=1
-			self.fn=None
-			self.logit("search... \n")
-			for i in range(5):
-				pdata=self.posdata["pos"+str(i)]
-				cnt=hexstr.count(pdata[0])
-				self.logit("test1."+str(i)+" =>"+str(cnt)+" "+pdata[0].replace(" ","")+" \n")
-				spos=0
-				if cnt!=1:
-					check=0
-				else:
-					spos=(hexstr.index(pdata[0])/3)+(len(pdata[0])/3)
-					for r in range(len(self.res)):
-						self.logit("res"+str(r+2)+" "+repr([sum([256**x[0]*ord(x[1]) for x in enumerate(map[spos+pdata[1+r*2]:spos+pdata[1+r*2]+2])]) ,sum([256**x[0]*ord(x[1]) for x in enumerate(map[spos+pdata[2+r*2]:spos+pdata[2+r*2]+2])])])+"\n")
-			for i in range(2):
-				cnt=hexstr.count(self.posdata["more"+str(i)][0])
-				if cnt!=1:check=0
-				self.logit("test2."+str(i)+" =>"+str(cnt)+" "+self.posdata["more"+str(i)][0].replace(" ","")+"\n")
-			self.logit("check : "+str(check)+"  [0=fail, 1=ok] \n")
-			if check:
-				self.fn=fn		
-			map.close()
-			f.close()		
-	def logit(self,txt):
-		self.ltext.config(state=Tk.NORMAL)
-		self.ltext.insert(Tk.END ,txt)
-		self.ltext.config(state=Tk.DISABLED)
-	def __init__(self, master):
-		self.res=[[1024,768],[1280,1024]]
-		self.fn=None
-		frame = Tk.Frame(master)
-		frame.pack()
-		#self.button = Tk.Button(frame, text="QUIT", fg="red", command=frame.quit)
-		#self.button.pack(side=Tk.LEFT)
-		self.fbutton = Tk.Button(frame, text='select Patrizer.exe', command=self.callbackfile)
-		self.fbutton.pack()
-		self.ltext = Tk.Text(frame)
-		self.ltext.pack()	
-		self.logit("Log\n")
-		self.nfile = Tk.Frame(frame)
-		self.flabel = Tk.Label(self.nfile,text="new Name:")
-		self.flabel.pack(side=Tk.LEFT)
-		self.fname = Tk.Entry(self.nfile)
-		self.fname.pack(side=Tk.LEFT)
-		self.nfile.pack()
-		self.res1 = Tk.Frame(frame)
-		self.lx1 = Tk.Label(self.res1, text="Res2: X-",)
-		self.lx1.pack(side=Tk.LEFT)
-		self.ex1 = Tk.Entry(self.res1)
-		self.ex1.pack(side=Tk.LEFT)
-		self.ly1 = Tk.Label(self.res1, text="Y-")
-		self.ly1.pack(side=Tk.LEFT)
-		self.ey1 = Tk.Entry(self.res1)
-		self.ey1.pack(side=Tk.LEFT)
-		self.res1.pack()
-		self.res2 = Tk.Frame(frame)
-		self.lx2 = Tk.Label(self.res2, text="Res3: X-",)
-		self.lx2.pack(side=Tk.LEFT)
-		self.ex2 = Tk.Entry(self.res2)
-		self.ex2.pack(side=Tk.LEFT)
-		self.ly2 = Tk.Label(self.res2, text="Y-")
-		self.ly2.pack(side=Tk.LEFT)
-		self.ey2 = Tk.Entry(self.res2)
-		self.ey2.pack(side=Tk.LEFT)
-		self.res2.pack()
-		self.fname.insert(Tk.END,"newPatrizier.exe")
-		self.ex1.insert(Tk.END,self.res[0][0])
-		self.ey1.insert(Tk.END,self.res[0][1])
-		self.ex2.insert(Tk.END,self.res[1][0])
-		self.ey2.insert(Tk.END,self.res[1][1])
-		self.l5 = Tk.Label(frame, text="-->")
-		self.l5.pack(side=Tk.LEFT)
-		self.fwrite = Tk.Button(frame, text="Go",fg="red", command=self.fwrite)
-		self.fwrite.pack(side=Tk.LEFT)
-		self.posdata=dict(
-		pos0=[" C7 44 24 4C 20 03 00 00 C7 44 24 50 58 02 00 00 EB 2A 3C 01 75 12 C7 44 24 4C",0,8,0x16,0x1e],
-		pos1=[" C7 44 24 18 20 03 00 00 C7 44 24 1C 58 02 00 00 EB 2C 83 FE 01 75 12 C7 44 24 18",0,8,0x17,0x1f],
-		pos2=[" C7 44 24 3C 20 03 00 00 C7 44 24 40 58 02 00 00 EB 22 C7 44 24 3C",0x12,0x1a,0,8],
-		pos3=[" C7 44 24 48 20 03 00 00 C7 44 24 4C 58 02 00 00 EB 2B 83 F8 01 75 12 C7 44 24 48",0,8,0x16,0x1e],
-		pos4=[" C7 44 24 24 20 03 00 00 C7 44 24 28 58 02 00 00 EB 2C 83 F8 01 75 12 C7 44 24 24",0,8,0x17,0x1f],
-		more0=[" E8 FB B0 08 00 8B 0D A4 B0 6C 00 E8 90 76 FD FF A1 80 B2 6D 00 3D 20 03 00 00 0F 84 62 01 00 00 3D",0,-1,0xb,-1],
-		more1=[" 90 90 90 90 90 90 90 90 90 90 90 A1 80 B2 6D 00 83 EC 1C 3D 20 03 00 00 53 55 56 57 8B F1 74 3C 3D",0,-1,7,-1],
-		)
-		self.files={'accelMap.ini':"",'screenGame.ini':"",'textures.ini':""}
-		for fn in self.files:
-			f=open("data"+os.sep+fn,"r")
-			self.files[fn]=f.readlines()
-			f.close
-		self.convini={
-		'screenGame.ini':{
-		0:[
-		['[ANIM41]',-3,lambda x:"Frame0=10 0 0 0 0 "+str(x[0]-284)+" 42 0\n"],
-		['[ANIM43]',-3,lambda x:"Frame0=8 0 0 0 0 284 "+str(284+(x[1]-768))+"\n"],
-		['[ANIM43]',-7,lambda x:"Pos="+str(x[0]-284)+" 600\n"]
-		],
-		1:[
-		['[ANIM42]',-3,lambda x:"Frame0=11 0 0 0 0 "+str(x[0]-284)+" 42\n"],
-		['[ANIM44]',-3,lambda x:"Frame0=9 0 0 0 0 284 "+str(424+(x[1]-1024))+"\n"],
-		['[ANIM44]',-7,lambda x:"Pos="+str(x[0]-284)+" 600\n"]
-		]
-		},
-		'accelMap.ini':{
-		0:[
-		['[SCREEN1]',-2,lambda x:"Size="+str(x[0])+" "+str(x[1])+"\n"],
-		['[ANIM1]',-3,lambda x:"Frame0=30023 0 0 0 0 "+str(x[0])+" "+str(x[1])+" 0\n"]
-		],
-		1:[
-		['[SCREEN2]',-2,lambda x:"Size="+str(x[0])+" "+str(x[1])+"\n"],
-		['[ANIM2]',-3,lambda x:"Frame0=30024 0 0 0 0 "+str(x[0])+" "+str(x[1])+" 0\n"]
-		]
-		},
-		'textures.ini':{
-		0:[['[TEX30023]',-4,lambda x:"OffsetNSize0=0 0 "+str(x[0])+" "+str(x[1])+"\n"]],
-		1:[['[TEX30024]',-4,lambda x:"OffsetNSize0=0 0 "+str(x[0])+" "+str(x[1])+"\n"]]
-		}
-		}
-	def fwrite(self):
-		check=1
-		if not self.fn:
-			self.logit("select patrizer.exe!\n")
-			check=0
-		if len([x for x in [self.ex1.get(),self.ex2.get(),self.ey1.get(),self.ey2.get()] if not re.search("^[0-9][0-9]*$",x)])==0 :
-			if not len([x for x in [self.ex1.get(),self.ex2.get(),self.ey1.get(),self.ey2.get()] if not 100<=int(x)<=9999])==0 :
-				self.logit("100<= Res X/Y <=9999 !\n")
-				check=0
-		else:
-			self.logit("only numbers ins Res X/Y!\n")
-			check=0
-		if not re.search("^[0-9a-zA-Z._-]*$",self.fname.get()):
-			self.logit("new filename = alphanumeric and -._ \n")
-			check=0
-		if check:
-			ppath=os.sep.join(os.path.realpath(self.fn).split(os.sep)[0:-1])
-			nfn=ppath+os.sep+self.fname.get()
-			self.res[0][0]=int(self.ex1.get())
-			self.res[0][1]=int(self.ey1.get())
-			self.res[1][0]=int(self.ex2.get())
-			self.res[1][1]=int(self.ey2.get())
-			shutil.copyfile(self.fn,nfn)
-			f=open(nfn, "r+b")
-			map = mmap.mmap(f.fileno(), 0)
-			hexstr=" "+" ".join(["%02X"%ord(x) for x in map])			
-			spos=[]
-			for i in range(5):
-				pdata=self.posdata["pos"+str(i)]
-			#	print pdata[0]
-				spos.append((hexstr.index(pdata[0])/3)+(len(pdata[0])/3))
-			for i in range(5):
-				pdata=self.posdata["pos"+str(i)]
-			#	print "%X"%spos[i]
-			#	print hexstr.count(pdata[0])
-				for r in range(len(self.res)):
-			#		print ["%02X"%ord(x) for x in map[spos[i]+pdata[1+r*2]:spos[i]+pdata[1+r*2]+2]] ,["%02X"%ord(x) for x in map[spos[i]+pdata[2+r*2]:spos[i]+pdata[2+r*2]+2]]
-					map[spos[i]+pdata[1+r*2]  ]=chr(self.res[r][0]%256)
-					map[spos[i]+pdata[1+r*2]+1]=chr(self.res[r][0]/256)
-					map[spos[i]+pdata[2+r*2]  ]=chr(self.res[r][1]%256)
-					map[spos[i]+pdata[2+r*2]+1]=chr(self.res[r][1]/256)
-			if self.res[1][0]>=1280:
-				self.logit("write data for res>=1280 \n")
-				for i in range(2):
-					pdata=self.posdata["more"+str(i)]
-					spos=(hexstr.index(pdata[0])/3)+(len(pdata[0])/3)
-					map[spos+pdata[3]  ]=chr(self.res[1][0]%256)
-					map[spos+pdata[3]+1]=chr(self.res[1][0]/256)
-				self.logit("resize pictures for res >=1280 \n")
-				if1 = Image.open("data"+os.sep+"Vollansichtskarte.bmp")
-				if1n = if1.resize((self.res[1][0], self.res[1][1]), Image.ANTIALIAS)
-				if1n.save(ppath+os.sep+"images"+os.sep+"Vollansichtskarte1280.bmp")
-				if2 = Image.open("data"+os.sep+"HauptscreenE.bmp")
-				if2n = if2.resize((284, 424-1024+self.res[1][1]), Image.ANTIALIAS)
-				if2n.save(ppath+os.sep+"images"+os.sep+"HauptscreenE1280.bmp")
-			map.close()
-			f.close()		
-			self.logit("writing to newfile done \n")
-			if not os.path.isdir(ppath+os.sep+'images'):
-				os.makedirs(ppath+os.sep+'images')
-				self.logit("creating images dir \n")
-			if not os.path.isdir(ppath+os.sep+'scripts'):
-				os.makedirs(ppath+os.sep+'scripts')
-				self.logit("creating script dir \n")
-			self.logit("write script files \n")
-			for fn in self.convini:
-				txt=""
-				for li in range(len(self.files[fn])):
-					found=-1
-					for r in range(len(self.res)):
-						for cn in self.convini[fn][r]:
-							if self.files[fn][li+cn[1]].startswith(cn[0]):				
-								txt+=cn[2](self.res[r])
-								#print files[fn][li],files[fn][li],cn[2](nres)
-								found=r
-					if found==-1:
-						txt+=self.files[fn][li]
-				f=open(ppath+os.sep+'scripts'+os.sep+fn,"w")
-				f.write(txt)
-				f.close()		
-			self.logit("done \n")
-root = Tk.Tk()
-app = App(root)
-root.mainloop()
+from PIL import Image
+
+project_root = Path(__file__).parent
+
+posdata = dict(
+    pos0=dict(
+        search_string=b"\xC7\x44\x24\x4C\x20\x03\x00\x00\xC7\x44\x24\x50\x58\x02\x00\x00\xEB\x2A\x3C\x01\x75\x12\xC7\x44\x24\x4C",
+        x1=0,
+        y1=8,
+        x2=0x16,
+        y2=0x1E,
+    ),
+    pos1=dict(
+        search_string=b"\xC7\x44\x24\x18\x20\x03\x00\x00\xC7\x44\x24\x1C\x58\x02\x00\x00\xEB\x2C\x83\xFE\x01\x75\x12\xC7\x44\x24\x18",
+        x1=0,
+        y1=8,
+        x2=0x17,
+        y2=0x1F,
+    ),
+    pos2=dict(
+        search_string=b"\xC7\x44\x24\x3C\x20\x03\x00\x00\xC7\x44\x24\x40\x58\x02\x00\x00\xEB\x22\xC7\x44\x24\x3C",
+        x1=0x12,
+        y1=0x1A,
+        x2=0,
+        y2=8,
+    ),
+    pos3=dict(
+        search_string=b"\xC7\x44\x24\x48\x20\x03\x00\x00\xC7\x44\x24\x4C\x58\x02\x00\x00\xEB\x2B\x83\xF8\x01\x75\x12\xC7\x44\x24\x48",
+        x1=0,
+        y1=8,
+        x2=0x16,
+        y2=0x1E,
+    ),
+    pos4=dict(
+        search_string=b"\xC7\x44\x24\x24\x20\x03\x00\x00\xC7\x44\x24\x28\x58\x02\x00\x00\xEB\x2C\x83\xF8\x01\x75\x12\xC7\x44\x24\x24",
+        x1=0,
+        y1=8,
+        x2=0x17,
+        y2=0x1F,
+    ),
+    more0=dict(
+        search_string=b"\xE8\xFB\xB0\x08\x00\x8B\x0D\xA4\xB0\x6C\x00\xE8\x90\x76\xFD\xFF\xA1\x80\xB2\x6D\x00\x3D\x20\x03\x00\x00\x0F\x84\x62\x01\x00\x00\x3D",
+        x1=0,
+        y1=-1,
+        x2=0xB,
+        y2=-1,
+    ),
+    more1=dict(
+        search_string=b"\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\x90\xA1\x80\xB2\x6D\x00\x83\xEC\x1C\x3D\x20\x03\x00\x00\x53\x55\x56\x57\x8B\xF1\x74\x3C\x3D",
+        x1=0,
+        y1=-1,
+        x2=7,
+        y2=-1,
+    ),
+)
+
+
+def read_resolution(path_to_patrizier_exe: Path) -> (int, int):
+    with open(path_to_patrizier_exe, "r+b") as file:
+        data: bytes = file.read()
+        for key, value in posdata.items():
+            if "pos" in key:
+                count = data.count(value["search_string"])
+                if count != 1:
+                    raise ValueError(f"Did not find {key}")
+
+                # find position after search string
+                spos = data.find(value["search_string"]) + len(value["search_string"])
+
+                for resolution_index in range(1, 3):
+                    x_res = int.from_bytes(
+                        data[spos + value[f"x{resolution_index}"] :][:2],
+                        byteorder="little",
+                    )
+                    y_res = int.from_bytes(
+                        data[spos + value[f"y{resolution_index}"] :][:2],
+                        byteorder="little",
+                    )
+                    logging.info(
+                        f"{key} resolution {resolution_index}: {x_res}x{y_res}"
+                    )
+            elif "more" in key:
+                count = data.count(value["search_string"])
+                if count != 1:
+                    raise ValueError(f"Did not find {key}")
+
+
+def change_resolution(
+    path_to_patrizier_exe: Path, width: int = 1024, height: int = 768
+):
+    width_small = 1024
+    height_small = 768
+    backup_file = path_to_patrizier_exe.with_suffix(".bak")
+    if backup_file.exists():
+        logging.warning(f"Keeping existing backup file: {backup_file}")
+    else:
+        logging.info(f"Backing up {path_to_patrizier_exe}")
+        shutil.copy(path_to_patrizier_exe, backup_file)
+
+    game_root_directory = path_to_patrizier_exe.parent
+
+    # patch new resolution into Patrizier II.exe
+    with open(path_to_patrizier_exe, "r+b") as file:
+        data: bytes = file.read()
+
+        for key, value in posdata.items():
+            if "pos" in key:
+                # write resolution
+                spos = data.index(value["search_string"]) + len(value["search_string"])
+                file.seek(spos + value["x1"])
+                file.write(int(width_small).to_bytes(length=2, byteorder="little"))
+                file.seek(spos + value["y1"])
+                file.write(int(height_small).to_bytes(length=2, byteorder="little"))
+                spos = data.index(value["search_string"]) + len(value["search_string"])
+                file.seek(spos + value["x2"])
+                file.write(int(width).to_bytes(length=2, byteorder="little"))
+                file.seek(spos + value["y2"])
+                file.write(int(height).to_bytes(length=2, byteorder="little"))
+
+            if width >= 1280 and "more" in key:
+                spos = data.index(value["search_string"]) + len(value["search_string"])
+                file.seek(spos + value["x2"])
+                file.write(int(width).to_bytes(length=2, byteorder="little"))
+
+    logging.info("Resize pictures for res >=1280")
+
+    images_directory = game_root_directory / "images"
+    if not images_directory.is_dir():
+        logging.info(f"Creating non existing directory {images_directory}")
+        images_directory.mkdir()
+
+    data_directory = project_root / "data"
+
+    if1 = Image.open(data_directory / "Vollansichtskarte.bmp")
+    if1n = if1.resize((width, height))
+    if1n.save(game_root_directory / "images" / "Vollansichtskarte1280.bmp")
+    if2 = Image.open(data_directory / "HauptscreenE.bmp")
+    if2n = if2.resize((284, 424 - 1024 + height))
+    if2n.save(game_root_directory / "images" / "HauptscreenE1280.bmp")
+
+    scripts_dir = game_root_directory / "scripts"
+    if not scripts_dir.is_dir():
+        logging.info(f"Creating non existing directory {scripts_dir}")
+        scripts_dir.mkdir()
+
+    def patch_script_file(filename: str, patches: list):
+        config = configparser.ConfigParser()
+
+        config.read(data_directory / filename, encoding="LATIN1")
+
+        for patch in patches:
+            config[patch["section"]][patch["key"]] = patch["value"]
+
+        with open(scripts_dir / filename, "w", encoding="LATIN1") as file:
+            config.write(file)
+
+    patch_script_file(
+        "screenGame.ini",
+        [
+            # patch resolution 1
+            #dict(
+            #    section="ANIM41",
+            #    key="Frame0",
+            #    value="10 0 0 0 0 " + str(width_small - 284) + " 42 0",
+            #),
+            #dict(
+            #    section="ANIM43",
+            #    key="Frame0",
+            #    value="8 0 0 0 0 284 " + str(284 + (height_small - 768)),
+            #),
+            #dict(section="ANIM43", key="Pos", value=str(width_small - 284) + " 600"),
+            # patch resolution 2
+            dict(section="ANIM42", key="Frame0", value=f"11 0 0 0 0 {width - 284} 42"),
+            dict(
+                section="ANIM44",
+                key="Frame0",
+                value=f"9 0 0 0 0 284 {424 + (height - 1024)}",
+            ),
+            dict(section="ANIM44", key="Pos", value=f"{width - 284} 600"),
+        ],
+    )
+
+    patch_script_file(
+        "accelMap.ini",
+        [
+            # patch resolution 1
+            dict(section="SCREEN1", key="Size", value=f"{width_small} {height_small}"),
+            #dict(
+            #    section="ANIM1",
+            #    key="Frame0",
+            #    value=f"30023 0 0 0 0 {width_small} {height_small} 0",
+            #),
+            # patch resolution 2
+            dict(section="SCREEN2", key="Size", value=f"{width} {height}"),
+            dict(
+                section="ANIM2", key="Frame0", value=f"30024 0 0 0 0 {width} {height} 0"
+            ),
+        ],
+    )
+
+    patch_script_file(
+        "textures.ini",
+        [
+            # patch resolution 1
+            #dict(
+            #    section="TEX30023",
+            #    key="OffsetNSize0",
+            #    value=f"0 0 {width_small} {height_small}",
+            #),
+            # path resolution 2
+            dict(section="TEX30024", key="OffsetNSize0", value=f"0 0 {width} {height}"),
+        ],
+    )
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    if len(sys.argv) == 2:
+        read_resolution(Path(sys.argv[1]))
+    elif len(sys.argv) == 4:
+        change_resolution(Path(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
